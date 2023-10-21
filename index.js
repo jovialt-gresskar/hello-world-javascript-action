5af3aac19a6855b325f99e179a736c22ebe4b9bd
Sindre Holtskog <sin-holt@online.no>
2023-10-21 15:07:30 +0200

    Add greeting from new repo name

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`Hello there. This action is now from new-jovialt-gresskar/hello-world-javascript-action` 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 } catch (error) {
